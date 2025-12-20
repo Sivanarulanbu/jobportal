b2f3a2a87bcdbea85e0ffe4d9af749031ea41de0
Sivanarul <krishnananbu99@gmail.com>
2025-12-20 12:34:42 +0530

    Update email settings for production environment

--- job_portal_backend/job_portal_backend/settings.py
+++ job_portal_backend/job_portal_backend/settings.py
@@ -74,12 +74,12 @@ TEMPLATES = [
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'krishnananbu99@gmail.com'
-EMAIL_HOST_PASSWORD = 'osyo xyua uvfq ggow'  # Your app password
-DEFAULT_FROM_EMAIL = 'Jobportal <krishnananbu99@gmail.com>'
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'krishnananbu99@gmail.com')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'osyo xyua uvfq ggow')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'Jobportal <krishnananbu99@gmail.com>')
 EMAIL_TIMEOUT = 30  # Increased timeout
 EMAIL_USE_SSL = False
 
