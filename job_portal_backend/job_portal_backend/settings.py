52f241e8529d3959da4f62a6ac35c5f1b48714c7
Sivanarul <krishnananbu99@gmail.com>
2025-12-20 15:03:39 +0530

    Allow EMAIL_USE_SSL configuration via env variables

--- job_portal_backend/job_portal_backend/settings.py
+++ job_portal_backend/job_portal_backend/settings.py
@@ -81,7 +81,7 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'krishnananbu99@gmail.com')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'osyo xyua uvfq ggow')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'Jobportal <krishnananbu99@gmail.com>')
 EMAIL_TIMEOUT = 30  # Increased timeout
-EMAIL_USE_SSL = False
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False') == 'True'
 
 DATABASES = {
     'default': dj_database_url.config(
